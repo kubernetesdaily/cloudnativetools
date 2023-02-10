e04d2a9c164488e6302fd37e2a246a117ec95301
Sangam Biradar <sangam14@users.noreply.github.com>
2023-02-10 13:13:40 +0530

    add

--- entries.js
+++ entries.js
@@ -21,8 +21,24 @@ const entries = [
 
   //b 
 
+  {
+	title: "breeze",
+	filename: "",
+	description: "Deploy a Production Ready Kubernetes Cluster with graphical interface",
+	tag: "Cluster Management",
+	github: "wise2c-devops/breeze",
+  },
+
 
   //c
+
+  {
+	title: "clusterman",
+	filename: "",
+	description: "Clusterman - Autoscale and Manage your compute clusters",
+	tag: "Cluster Management",
+	github: "Yelp/clusterman",
+},
 
   //d
 
@@ -33,30 +49,119 @@ const entries = [
 
   // h 
 
+  {
+	title: "harvester",
+	filename: "",
+	description: "Open source hyperconverged infrastructure (HCI) software",
+	tag: "Cluster Management",
+	github: "harvester/harvester",
+},
+
+
   // i 
 
   // g 
 
   // k
 
+  {
+	title: "kind",
+	filename: "",
+	description: "Kubernetes IN Docker - local clusters for testing Kubernetes",
+	tag: "Cluster Management",
+	github: "kubernetes-sigs/kind",
+},
+
+  {
+	title: "kOps",
+	filename: "",
+	description: "Kubernetes Operations (kOps) - Production Grade k8s Installation, Upgrades and Management",
+	tag: "Cluster Management",
+	github: "kubernetes/kops",
+},
+
+
+{
+	title: "kubesphere",
+	filename: "",
+	description: "The container platform tailored for Kubernetes multi-cloud, datacenter, and edge management ",
+	tag: "Cluster Management",
+	github: "kubesphere/kubesphere",
+},
+
+{
+	title: "kubespray",
+	filename: "",
+	description: "Deploy a Production Ready Kubernetes Cluster ",
+	tag: "Cluster Management",
+	github: "kubernetes-sigs/kubespray",
+},
+
+{
+	title: "kubicorn",
+	filename: "",
+	description: "Simple, cloud native infrastructure for Kubernetes",
+	tag: "Cluster Management",
+	github: "kubicorn/kubicorn",
+},
+
+{
+	title: "kubeone",
+	filename: "",
+	description: "Kubermatic KubeOne automate cluster operations on all your cloud, on-prem, edge, and IoT environments",
+	tag: "Cluster Management",
+	github: "kubermatic/kubeone",
+},
+
+{
+	title: "kubeone",
+	filename: "",
+	description: "Kubermatic KubeOne automate cluster operations on all your cloud, on-prem, edge, and IoT environments",
+	tag: "Cluster Management",
+	github: "kubermatic/kubeone",
+},
+
+
   // l
 
-
+  
 
   // M
-	{
-		title: "Monokle",
-		filename: "learn-Monokle.html",
-		description: "🧐 Monokle streamlines the process of creating, analyzing, and deploying Kubernetes configurations by providing a unified visual tool for authoring YAML manifests, validating policies, and managing live clusters.",
-		tag: "Cluster Management",
-		github: "kubeshop/monokle",
-	}
+
+  {
+	title: "matchbox",
+	filename: "",
+	description: "Network boot and provision Fedora CoreOS and Flatcar Linux clusters",
+	tag: "Cluster Management",
+	github: "poseidon/matchbox",
+},
+  {
+	title: "metalk8s",
+	filename: "",
+	description: "An opinionated Kubernetes distribution with a focus on long-term on-prem deployments",
+	tag: "Cluster Management",
+	github: "scality/metalk8s",
+},
+{   title: "Monokle",
+    filename: "",
+	description: "Monokle streamlines the process of creating, analyzing, and deploying Kubernetes configurations by providing a unified visual tool for authoring YAML manifests, validating policies, and managing live clusters.",
+	tag: "Cluster Management",
+	github: "kubeshop/monokle",
+},
 
 	//N 
 
 
 	// O 
 
+	{
+		title: "k0s",
+		filename: "",
+		description: "k0s - The Zero Friction Kubernetes ",
+		tag: "Cluster Management",
+		github: "k0sproject/k0s",
+	},
+	
 
 	// P 
 
@@ -66,7 +171,37 @@ const entries = [
 
 	// S
 
+	{
+		title: "silver-surfer",
+		filename: "",
+		description: "sealos is a Kubernetes distribution, a general-purpose cloud operating system for managing cloud-native applications. ",
+		tag: "Cluster Management",
+		github: "labring/sealos",
+	},
+
+	
+
+	{
+		title: "silver-surfer",
+		filename: "",
+		description: "An OpenSource project to check ApiVersion compatibility and provide Migration path for Kubernetes objects when upgrading Kubernetes to latest versions. ",
+		tag: "Cluster Management",
+		github: "devtron-labs/silver-surfer",
+	},
+
+
+
 	// T 
+
+	{
+		title: "typhoon",
+		filename: "",
+		description: "Minimal and free Kubernetes distribution with Terraform ",
+		tag: "Cluster Management",
+		github: "poseidon/typhoon",
+	}
+
+	
 
 	// U 
 
@@ -83,6 +218,6 @@ const entries = [
 	// Y 
 
 	// Z 
-	
+
 
 ];
