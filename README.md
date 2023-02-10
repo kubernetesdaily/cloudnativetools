ef6903831dcf545fdfcfb4df19dea18c2370727e
Sangam Biradar <sangam14@users.noreply.github.com>
2023-02-10 08:52:05 +0530

    new list

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Kubernetes-CloudNative-Toolkits
-List of Awesome K8s &amp; CloudNative Toolkits 
+[![Ultimate Kubenetes & CloudNative Toolkits ](./meta/Kubernetes%20%26%20CloudNative%20Toolkit.jpg?raw=true "Ultimate Kubenetes & CloudNative Toolkits ")]()
+
+
+
+### [View Submissions]()
+
+## Rules
+
+ 
+## How to Submit
+ 1. Fork the repository.
+ 2. Add your html file to the `/entries` directory.
+ 3. Edit the `entries.js` file in the root, with your information for the entry.
+ 4. Commit to your forked repo.
+ 5. Make a pull request to master from your forked repo.
+
+ 
+
+
+_If you write a blog or post about the challenge, feel free to add to the list_
+ 
+
+
+ ## Useful Links
+
+
+## Contributors
+
---
+++ entries.js
@@ -0,0 +1,30 @@
+/**
+ * Every entry must include a title and filename, the rest are optional.
+ * For simplicity in merges, please add to the bottom of the object.
+ *
+ * interface
+ * {
+ *     title: string // display name
+ *     filename: string // the name of your HTML file which contain learning resources
+ *     description?: string // description that will be listed with your entry
+ *     author?: string // your name/tag that will be listed
+ *     github?: string // username on github that will display a link to your repo
+ *     compatibleBrowsers?: array // browsers that this page is compatible with
+ * }
+ */
+
+
+
+
+const entries = [
+  //A
+
+  // M
+	{
+		title: "Monokle",
+		filename: "learn-Monokle.html",
+		description: "🧐 Monokle streamlines the process of creating, analyzing, and deploying Kubernetes configurations by providing a unified visual tool for authoring YAML manifests, validating policies, and managing live clusters.",
+		tag: "Cluster Management",
+		github: "kubeshop/monokle",
+	}
+];
