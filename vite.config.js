b8b53fecb8a4754b258c8db4d9fdbb49ee64c910
Sangam Biradar <sangam14@users.noreply.github.com>
2023-03-31 05:41:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
+// https://vitejs.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./"
   plugins: [react()],
-  
 })
