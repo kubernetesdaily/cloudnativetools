fa8e3f5b783990c60ad570069f144c487dcad656
Sangam Biradar <sangam14@users.noreply.github.com>
2023-03-31 05:51:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-// https://vitejs.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./"
-  plugins: [react()],
+  base: 'https://kubedaily.com/Kubernetes-CloudNative-Toolkits/',
+  plugins: [react()]
 })
