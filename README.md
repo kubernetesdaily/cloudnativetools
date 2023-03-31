a2e534f4e14255bce9085481c17dd69fa2a63403
Jelsinge Prajwal <jelsinge.prajwal@gmail.com>
2023-03-31 17:29:56 +0000

    added updated repo links && Readme File

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 4. run `npm install`
 5. run `npm run dev` to start development server
 6. run `npm run build` to build the project
+
+## To add a new entry 
+1. Go to [Entries.js](https://github.com/Jelsingeprajwal/Kubedaily-kubetools/blob/main/src/data/entries.js)
+2. Add your entry with all the details to the end of the `entries.js` file
+3. Create a PR
+4. You are done 🔥🔥
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,8 +17,8 @@ function Hero() {
               Submit Your Favourite Open Source Tool
             </h3>
             <div className="flex flex-col md:flex-row justify-center gap-5 mt-4">
-              <Button text={"Submit an Entry"} link={"https://github.com/Jelsingeprajwal/Kubedaily-kubetools/blob/main/src/data/entries.js"} />
-              <Button text={"Github"} link={"https://github.com/Jelsingeprajwal/Kubedaily-kubetools"} />
+              <Button text={"Submit an Entry"} link={"https://github.com/sangam14/Kubernetes-CloudNative-Toolkits/blob/main/src/data/entries.js"} />
+              <Button text={"Github"} link={"https://github.com/sangam14/Kubernetes-CloudNative-Toolkits"} />
               <Button
                 text={"Join Us on Discord"}
                 link={"https://discord.com/invite/rEvr7vq"}
