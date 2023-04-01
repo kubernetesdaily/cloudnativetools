76e0e21dd49573c7e0c64ebddaf825a2bff64587
jelsinge prajwal <jelsinge.prajwal@gmail.com>
2023-04-02 00:16:04 +0530

    gitignore file update & Github button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import { React } from "react";
-
+import GitHubButton from 'react-github-btn'
 function Footer() {
-    return <div className="bg-bgPrimary w-full px-6 py-6  max-w-4xl mx-auto">
-        Designed by <a href="https://github.com/Jelsingeprajwal" target={"_blank"} className="font-semibold">Jelsinge Prajwal</a>
+    return <div className="bg-bgGray w-[100%]  py-6 lg:px-44 px-8   mx-auto">
+
+        <div className="flex justify-between">
+            <div>
+
+                Designed by <a href="https://github.com/Jelsingeprajwal" target={"_blank"} className="font-semibold">Jelsinge Prajwal</a>
+            </div>
+            <div>
+                {/* <!-- Place this tag where you want the button to render. --> */}
+                <GitHubButton href="https://github.com/sangam14/Kubernetes-CloudNative-Toolkits" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star sangam14/Kubernetes-CloudNative-Toolkits on GitHub">Star</GitHubButton>
+            </div>
+        </div>
+
     </div>
 }
 export default Footer
